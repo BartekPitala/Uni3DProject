5f677a55b591891399c72f4327ef24379ef682d4
rxrt8 <michal25kos@gmail.com>
2017-07-28 16:25:28 +0200

    [Michał] add simple enemy (with some mistakes, which I wrote in CHANGELOG)

---
+++ FPS in Unity3D/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public float walkSpeed = 5.0f;
+    public float attackDistance = 2.0f;
+    public float attackDemage = 10.0f;
+    public float attackDelay = 1.0f;
+    public float hp = 20.0f;
+    public Transform[] transforms;
+
+    private float timer = 0;
+    private string currentState;
+    private Animator animator;
+    private AnimatorStateInfo stateInfo;
+
+    void Start()
+    {
+        animator = transforms[0].GetComponent<Animator>();
+        currentState = "";
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag.Equals("Player") && hp > 0)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(other.transform.position - transform.position);
+            float oryginalX = transform.rotation.x;
+            float oryginalZ = transform.rotation.z;
+
+            Quaternion finalRotation = Quaternion.Slerp(transform.rotation, targetRotation, 5.0f * Time.deltaTime);
+            finalRotation.x = oryginalX;
+            finalRotation.z = oryginalZ;
+            transform.rotation = finalRotation;
+
+            float distance = Vector3.Distance(transform.position, other.transform.position);
+            if (distance > attackDistance && !stateInfo.IsName("Base Layer.wound"))
+            {
+                animationSet("run");
+                transform.Translate(Vector3.forward * walkSpeed * Time.deltaTime);
+            }
+            else
+            {
+                if (timer <= 0)
+                {
+                    animationSet("attack0");
+                    other.SendMessage("takeHit", attackDemage);
+                    timer = attackDelay;
+                }
+            }
+
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            animationSet("idle0");
+        }
+    }
+
+    private void animationSet(string animationToPlay)
+    {
+        stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        animationReset();
+
+        if (currentState == "")
+        {
+            currentState = animationToPlay;
+
+            if (stateInfo.IsName("Base Layer.run") && currentState != "run")
+            {
+                animator.SetBool("runToIdle0", true);
+            }
+
+            string state = "idle0To" + currentState.Substring(0, 1).ToUpper() + currentState.Substring(1);
+            animator.SetBool(state, true);
+            currentState = "";
+        }
+    }
+
+    private void animationReset()
+    {
+        if (!stateInfo.IsName("Base Layer.idle0"))
+        {
+            animator.SetBool("idle0ToIdle1", false);
+            animator.SetBool("idle0ToWalk", false);
+            animator.SetBool("idle0ToRun", false);
+            animator.SetBool("idle0ToWound", false);
+            animator.SetBool("idle0ToSkill0", false);
+            animator.SetBool("idle0ToAttack1", false);
+            animator.SetBool("idle0ToAttack0", false);
+            animator.SetBool("idle0ToDeath", false);
+        }
+        else
+        {
+            animator.SetBool("runToIdle0", false);
+        }
+    }
+
+    void takeHit(float demage)
+    {
+        hp -= demage;
+        if (hp <= 0)
+        {
+            animationSet("death");
+        }
+        else
+        {
+            animator.CrossFade("wound", 0.5f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ FPS in Unity3D/README.md
@@ -0,0 +1,27 @@
+﻿#CHANGELOG
+
+[ !!! WYPELNIAJCIE TO, BEDZIE NAM LATWIEJ SPAMIETAC NA CO ZWROCIC UWAGE JESZCZE (moze wypelnijcie nawet wstecz jak pamietacie)!!!]
+
+[Bartek] 25.07.2017 - Zrobilem party 9 i 10, ale nie do konca działa.
+
+                      Granat sie rzuca, ale z okolo 2 sek. opoznieniem.
+                      
+                      Dziala animacja wybuchu i dzwięk też, lecacy granat widac ale bardzo szybko znika (?)
+                      
+                      Dodałem mozliwosc strzelania auto z mp5 poprzez przytrzymanie przycisku myszy, ale bardzo szybko te strzały są po sobie, 
+                      praktycznie 1 sekunda i caly magazynek out... probowalem pozmieniać parametry opoznienia po strzale itp ale wydają sie w
+                      wogole nie miec wplywu na to....
+                      
+                      Idealnie dziala zblizanie i oddalanie prawym przyciskiem myszki, bardzo fajnie to wyszło, płynne przejście i wogóle.
+                      
+                      Dodalem teksture dziury po kuli, ale nie udalo mi sie ustawic jej transparentnosci, wiec wyswietla sie z otoczka bialego prostokata :/
+                      szukalem jakichs transparency, alpha channel itp ale nic zdaje sie nie dzialac...teraz sie zastanawiam, czy moze to biale tlo nie jest 
+                      transparentne, tylko po prostu jest to "biale tło" i trzeba inne zdjecie po prostu dać (?).
+                      
+                      Te dziury sie tez pojawiaja troche obok celownika, dziwne to jest ale to musi byc zwiazane
+                      mysle z jakimis poprzednimi czesciami tutoriala i tym gdzie "patrzy" dokladnie kamera albo cos.....
+                      
+
+
+[Michał] zrobiłem wroga, choć nie porusza się w stałej płaszczyźnie y. Inny problem to brak ravast hit triggers, ale chyba sobie z nim poradziłem. Jak wróg nas uderza trzeba wyświetlić tę teksturę z krwią.
+Dodam potem dźwięk jak zbiera się broń oraz napisy ze stanem amunicji. 
