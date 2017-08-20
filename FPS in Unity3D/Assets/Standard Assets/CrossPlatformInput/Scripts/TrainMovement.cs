358dbef19e33eefee7389d100d4b55ee80d176bf
Testowy  Uzytkownik <pitermarszalek@o2.pl>
2017-08-20 14:15:47 +0200

    [PETRO] Finished subway

---
+++ FPS in Unity3D/Assets/Standard Assets/CrossPlatformInput/Scripts/TrainMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainMovement : MonoBehaviour
+{
+    private bool forward;
+    public AudioClip trainSound;
+    // Use this for initialization
+    void Start () {
+        forward = true;
+        GetComponent<AudioSource>().clip = trainSound;
+    }
+	// Update is called once per frame
+	void Update () {
+
+
+       // GetComponent<AudioSource>().Play();
+        if (transform.position.y <= 14.24 && transform.position.y >= 14.19 && forward)
+        {
+            transform.Translate(0f, -0.4f, 0f);
+            if(transform.position.y<=14.19)
+            forward = false;
+
+        }
+        if (transform.position.y <= 14.24 && transform.position.y >= 14.18 && !forward)
+        {
+            transform.Translate(0f, 0.4f, 0f);
+            if(transform.position.y>=14.235)
+            forward = true;
+        }
+
+    }
+}
+
