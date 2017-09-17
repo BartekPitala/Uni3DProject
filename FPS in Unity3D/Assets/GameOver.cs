d4c8c0e8b9a4c7c9921eff71e69a33d335ad409a
Testowy  Uzytkownik <pitermarszalek@o2.pl>
2017-09-17 14:44:40 +0200

    Customized cursor and buttons font. Prepared buttons to change between scenes properly. Now waiting for the official logo.

--- FPS in Unity3D/Assets/GameOver.cs
+++ FPS in Unity3D/Assets/GameOver.cs
@@ -6,15 +6,38 @@ public class GameOver : MonoBehaviour {
 
 
     public Texture GameOverTexture;
-	// Use this for initialization
+	public float buttonWidth = 50;
+    public float buttonHeight = 100;
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    private GUIStyle mainStyle = new GUIStyle();
+    public Font mainFont;
 
-	void OnGUI(){
+    void OnGUI(){
         GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), GameOverTexture);
-	}
+        
+        if (GUI.Button(new Rect(Screen.width*0.4f-buttonWidth / 2.0f, Screen.height*0.6f, buttonWidth, buttonHeight), "PLAY AGAIN", mainStyle))
+        {
+            Application.LoadLevel("scene1");
+        }
+        if (GUI.Button(new Rect(Screen.width * 0.6f-buttonWidth / 2.0f, Screen.height * 0.6f, buttonWidth, buttonHeight), "MAIN MENU", mainStyle))
+        {
+            Application.LoadLevel("Menu");
+        }
 
-	void Start () {
-		
-	}
+
+    }
+
+	 void Start()
+    {
+        buttonWidth = Screen.width/7.3f;
+        buttonHeight = Screen.height/7.3f;
+        Cursor.visible = true;
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        mainStyle.font = mainFont;
+        mainStyle.fontSize = 20;
+    }
 	
 	// Update is called once per frame
 	void Update () {
--- FPS in Unity3D/Assets/Menu.cs
+++ FPS in Unity3D/Assets/Menu.cs
@@ -7,11 +7,16 @@ public class Menu : MonoBehaviour
 {
 
     public Texture GameLogo;
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
     public float buttonWidth = 500;
     public float buttonHeight = 1000;
     private float buttonMargin = 20;
     public GUISkin skin;
     public GUIStyle disabledButton;
+    private GUIStyle mainStyle = new GUIStyle();
+    public Font mainFont;
 
 
     void OnGUI()
@@ -21,16 +26,16 @@ public class Menu : MonoBehaviour
 
 
         GUI.BeginGroup(new Rect(Screen.width/10, Screen.height/1.6f, buttonWidth, (buttonHeight + buttonMargin) * 3));
-        if (GUI.Button(new Rect(0, 0, buttonWidth, buttonHeight), "New Game"))
+        if (GUI.Button(new Rect(0, 0, buttonWidth, buttonHeight), "NEW GAME", mainStyle))
         {
             //SceneManager.LoadScene("scene1");
             Application.LoadLevel("scene1");
         }
-        if (GUI.Button(new Rect(0, 0 + buttonHeight + buttonMargin, buttonWidth, buttonHeight), "Options"))//, disabledButton))
+        if (GUI.Button(new Rect(0, 0 + buttonHeight + buttonMargin, buttonWidth, buttonHeight), "OPTIONS", mainStyle))//, disabledButton))
         {
 
         }
-        if (GUI.Button(new Rect(0, 0 + (buttonHeight + buttonMargin) * 2, buttonWidth, buttonHeight), "Exit"))
+        if (GUI.Button(new Rect(0, 0 + (buttonHeight + buttonMargin) * 2, buttonWidth, buttonHeight), "EXIT", mainStyle))
         {
             Application.Quit();
         }
@@ -45,6 +50,10 @@ public class Menu : MonoBehaviour
         buttonWidth = (buttonWidth * Screen.width) / 1920;
         buttonHeight = (buttonHeight * Screen.height) / 1080;
         buttonMargin = (buttonMargin * Screen.height) / 1080;
+        Cursor.visible = true;
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        mainStyle.font = mainFont;
+        mainStyle.fontSize = 20;
     }
 
     // Update is called once per frame
